d8cc937cd8009862d7b35e449888ea75988cb7f9
huydao250606 <110663322+huydao250606@users.noreply.github.com>
2025-08-03 23:44:35 +0700

    Add files via upload

---
+++ PTIT_CNTT4_IT104/PTIT_CNTT4_IT104_Session01/PTIT_CNTT4_IT104_Session01_Bai03.js
@@ -0,0 +1,10 @@
+// Tạo mảng các sinh viên
+const students = [
+  { id: 1, name: "Nguyễn A" },
+  { id: 2, name: "Trần B" },
+  { id: 3, name: "Lê C" },
+];
+
+students.forEach((student) => {
+  console.log(`Xin chào ${student.name}! Mã số: ${student.id}.`);
+});
